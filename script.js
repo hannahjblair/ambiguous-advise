4c6e85ff194cb4b6e83a870db7843073f38bbca2
Hannah Blair <hannahjblair@protonmail.com>
2023-08-22 01:18:33 -0400

    added quotes function to display a random log lady quote when the submit botton is pressed and display it below the question/message that the user inputs

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function pr() {
 function clearInput() {
     document.getElementById("Form").reset();
   }
+
+  // adapted from https://codepen.io/mina_mana_mu/pen/bGdNpdZ
+  var sayings = [
+    "Behind all things are reasons. Reasons can even explain the absurd.",
+    "Watch, and see what life teaches",
+    "All that we see in this world is based on someon's ideas. Some ideas are destructive, some are constructive. Some ideas can arrive in the forms of a dream. I can say it again: Some ideas arrive in the form of a dream",
+    "There is a sadness in this world, for we are ignorant of many things. Yes- we are ignorant of many beautiful things. Thongs like the truth.So sadness in our ignorance is very real. The tears are real. What is this thing called a tear? There are even tiny ducts- tear ducts- to produce thhese tears should the sadness occur.",
+    
+
+  ]
+
+  function quotes(){
+  var randomNumber = Math.floor (Math.random()* (sayings.length));
+  document.getElementById('quoteDisplay').innerHTML = sayings [randomNumber];
+  }
