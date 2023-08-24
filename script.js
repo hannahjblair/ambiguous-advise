fa849aed6d0073b9e5e3fb83e2f7b70299b7780c
Hannah Blair <hannahjblair@protonmail.com>
2023-08-23 22:46:16 -0400

    fixed typo in one of the log lady's quotes

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function clearInput() {
     "Behind all things are reasons. Reasons can even explain the absurd.",
     "Watch, and see what life teaches",
     "All that we see in this world is based on someone's ideas. Some ideas are destructive, some are constructive. Some ideas can arrive in the forms of a dream. I can say it again: Some ideas arrive in the form of a dream",
-    "There is a sadness in this world, for we are ignorant of many things. Yes- we are ignorant of many beautiful things. Thongs like the truth.So sadness in our ignorance is very real. The tears are real. What is this thing called a tear? There are even tiny ducts- tear ducts- to produce thhese tears should the sadness occur.",
+    "There is a sadness in this world, for we are ignorant of many things. Yes- we are ignorant of many beautiful things. Things like the truth.So sadness in our ignorance is very real. The tears are real. What is this thing called a tear? There are even tiny ducts- tear ducts- to produce thhese tears should the sadness occur.",
     "Then the day when the sadness comes. Then we ask, 'Will the sadness which makes me cry, will the sadness that makes me cry my heart out, will it ever end?' The answer, of course, is yes. One day, the sadness will end.",
     "Even the ones who laugh are sometimes caught without an answer. These creatures who introduce themselves, but we swear we have met them somewhere before, yes? Look in the mirror. What do you see? Is it a dream, or a nightmare? Are we being introduced against our will? Are they mirrors? I can see the smoke. I can smell the fire. The battle is drawing nigh.",
     "I play my part on life's stage. I tell what I can to form the perfect answer. But that answer cannot come before all are ready to hear, so I tell what I can to form the perfect answer. Sometimes my anger at the fire is evident. Sometimes it is not anger, really – it may appear as such, but could it be a clue? The fire I speak of is not a kind fire.",
@@ -24,7 +24,7 @@ function clearInput() {
     "Balance is the key. Balance is the key to many things. Do we understand balance? The word 'balance' has seven letters. Seven is difficult to balance, but not impossible – we are able to divide. There are, of course, the pros and cons of division.",
     "So now the sadness comes. The revelation. There is a depression after an answer is given. It was almost fun not knowing. Yes, now we know. At least we know what we sought in the beginning. But there is still the question, why? And this question will go on and on until the final answer comes. Then the knowing is so full there is no room for questions.",
     "Complications set in--yes, complications. How many times have we heard: 'it's simple'. Nothing is simple. We live in a world where nothing is simple. Each day, just when we think we have a handle on things, suddenly some new element is introduced and everything is complicated once again.",
-    
+
 
   ]
 
